6f09ade0c08a791aa232391159cd6bfb1802378b
Charlito Mecarez Jr <70713746+developedbycharkun@users.noreply.github.com>
2022-03-18 16:50:12 +0800

    update

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const api_key = `4fb26edefa3844e4ad2455ef77fc0d13`;
-const api_url = `https://api.spoonacular.com/recipes/random?apiKey=${api_key}&number=2`;
+// const api_key = `4fb26edefa3844e4ad2455ef77fc0d13`;
+// const api_url = `https://api.spoonacular.com/recipes/random?apiKey=${api_key}&number=2`;
 
-const recipe_container = document.querySelector('.recipe_container');
+// const recipe_container = document.querySelector('.recipe_container');
 
-// Defining async function
-async function getapi(api_url) {
-	// Storing response
-	const response = await fetch(api_url);
+// // Defining async function
+// async function getapi(api_url) {
+// 	// Storing response
+// 	const response = await fetch(api_url);
 
-	// Storing data in form of JSON
-	var data = await response.json();
-	const data_recipes = data.recipes;
+// 	// Storing data in form of JSON
+// 	var data = await response.json();
+// 	const data_recipes = data.recipes;
 
-	data_recipes.forEach((recipe) => {
-		// Creating divs
-		const create_div = document.createElement('div');
+// 	data_recipes.forEach((recipe) => {
+// 		// Creating divs
+// 		const create_div = document.createElement('div');
 
-		console.log(recipe);
+// 		console.log(recipe);
 
-		create_div.innerHTML = `
-            <p class="recipe_title">${recipe.title}</p>
-            <img class="recipe_image" src=${recipe.image} />
-        `;
+// 		create_div.innerHTML = `
+//             <p class="recipe_title">${recipe.title}</p>
+//             <img class="recipe_image" src=${recipe.image} />
+//         `;
 
-		recipe_container.appendChild(create_div);
-	});
-}
-// Calling that async function
-getapi(api_url);
+// 		recipe_container.appendChild(create_div);
+// 	});
+// }
+// // Calling that async function
+// getapi(api_url);
